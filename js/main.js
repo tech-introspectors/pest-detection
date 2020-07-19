8bd9b8f74fd7b820094a1569e4a56e91b219d821
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-07-19 12:37:25 +0530

    Navbar is fixed in mobile view

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-$(window).ready(function () {
-    $("#menuToggle i").click(function () {
-        $("#menuContent").slideToggle().css({
-            cursor: "poiner"
-        });
+$(function () {
+    menu = $('#menuContent');
+
+    $('#menuToggle').on('click', function (e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+
+    $(window).resize(function () {
+        var w = $(this).width();
+        if (w > 480 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+
+    $('#menuContent ul li').on('click', function (e) {
+        var w = $(window).width();
+        if (w < 480) {
+            menu.slideToggle();
+        }
     });
 });
