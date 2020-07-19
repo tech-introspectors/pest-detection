6facd906809859b094ac2ff59cf7b94082e6912d
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-07-20 00:32:48 +0530

    Added new navigation for mobile view

--- js/main.js
+++ js/main.js
@@ -1,23 +1,30 @@
-$(function () {
-    menu = $('#menuContent');
+//$(function () {
+//    menu = $('#menuContent');
+//
+//    $('#button').on('click', function (e) {
+//        e.preventDefault();
+//        $(this).css("cursor", "pointer");
+//        menu.slideToggle();
+//    });
+//
+//    $(window).resize(function () {
+//        var w = $(this).width();
+//        if (w > 480 && menu.is(':hidden')) {
+//            menu.removeAttr('style');
+//        }
+//    });
+//
+//    $('#menuContent ul li').on('click', function (e) {
+//        var w = $(window).width();
+//        if (w < 480) {
+//            menu.slideToggle();
+//        }
+//    });
+//});
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+}
 
-    $('#button').on('click', function (e) {
-        e.preventDefault();
-        $(this).css("cursor", "pointer");
-        menu.slideToggle();
-    });
-
-    $(window).resize(function () {
-        var w = $(this).width();
-        if (w > 480 && menu.is(':hidden')) {
-            menu.removeAttr('style');
-        }
-    });
-
-    $('#menuContent ul li').on('click', function (e) {
-        var w = $(window).width();
-        if (w < 480) {
-            menu.slideToggle();
-        }
-    });
-});
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
